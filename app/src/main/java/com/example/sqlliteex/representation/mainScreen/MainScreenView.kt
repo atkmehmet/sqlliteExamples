05718c836aee20f961505fd2bd0cdd8e2beb8888
atkmehmet <durmazmehmet47@gmail.com>
2023-12-21 00:37:15 +0300

    some changes make it

--- app/src/main/java/com/example/sqlliteex/representation/mainScreen/MainScreenView.kt
+++ app/src/main/java/com/example/sqlliteex/representation/mainScreen/MainScreenView.kt
@@ -1,5 +1,9 @@
 package com.example.sqlliteex.representation.mainScreen
+import androidx.compose.runtime.mutableStateOf
 
+import androidx.compose.runtime.getValue
+
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.sqlliteex.ApplicationContext
@@ -9,13 +13,35 @@ import com.example.sqlliteex.data.local.PersonRecordDao
 import kotlinx.coroutines.launch
 
 class mainScreenView:ViewModel() {
-    val personRecordDao=BookDatabase.getPersonDao(ApplicationContext.getAppContext())
-
+   private var personRecordDao=BookDatabase.getPersonDao(ApplicationContext.getAppContext())
+   private var _state by mutableStateOf(mainScreenSatete())
+    val state:mainScreenSatete
+        get() = _state
     init {
         viewModelScope.launch {
             personRecordDao.insertPerson(PersonEntity(0,"Mehmet","Durmaz"))
         }
 
+    }
+
+    fun onEvent(event: mainScreenEvent){
+        when(event){
+            is mainScreenEvent.personName ->{
+             _state = _state.copy(
+                 personName = event.name
+             )
+            }
+            is mainScreenEvent.personSurname->{
+                _state = _state.copy(
+                    personSurname = event.surname
+                )
+            }
+            is mainScreenEvent.personCommit->{
+                viewModelScope.launch {
+                 personRecordDao.insertPerson(PersonEntity(0,_state.personName,_state.personSurname))
+                }
+            }
+        }
     }
 
 }
--- app/src/main/java/com/example/sqlliteex/representation/mainScreen/compenent/mainScreen.kt
+++ app/src/main/java/com/example/sqlliteex/representation/mainScreen/compenent/mainScreen.kt
@@ -2,15 +2,28 @@ package com.example.sqlliteex.representation.mainScreen.compenent
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.example.sqlliteex.representation.mainScreen.mainScreenEvent
 import com.example.sqlliteex.representation.mainScreen.mainScreenView
 
 @Composable
-fun mainScreen(view: mainScreenView=mainScreenView()) {
+fun mainScreen(view: mainScreenView= mainScreenView()) {
 
     Column(modifier = Modifier.fillMaxSize()) {
-         Text(text = "Hello World")
+
+
+
+        TextField(value =view.state.personName ,
+            onValueChange ={view.onEvent(mainScreenEvent.personName(it))},
+            placeholder = {"Please name Value"} )
+
+        TextField(value = view.state.personSurname, onValueChange ={view.onEvent(mainScreenEvent.personSurname(it))} )
+        Button(onClick = { view.onEvent(mainScreenEvent.personCommit) }) {
+            
+        }
     }
 }
