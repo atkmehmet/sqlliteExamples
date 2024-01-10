16b58476130f288b2600f796e4817bca9283ff4d
atkmehmet <durmazmehmet47@gmail.com>
2024-01-11 00:06:57 +0300

    done

--- app/src/main/java/com/example/sqlliteex/representation/mainScreen/dropScreen.kt
+++ app/src/main/java/com/example/sqlliteex/representation/mainScreen/dropScreen.kt
@@ -1,9 +1,13 @@
 package com.example.sqlliteex.representation.mainScreen
 
 import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+
+
 import androidx.compose.foundation.layout.Column
+
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -28,6 +32,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.sqlliteex.data.local.ReadBookEntity
@@ -41,27 +46,34 @@ import kotlinx.coroutines.flow.take
 @Composable
 fun MyDropdownMenuScreen(view: mainScreenView ) {
 
+//test(view)
+    val list=view.listBook.collectAsState(initial = emptyList())
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        // Other content above LazyColumn
+        AddPerson(view = view)
+        DropMenu(view = view)
+        AddBook(view = view)
+        LazyColumn(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+        ) {
+            items(list.value) { item ->
+                // LazyColumn items
+                Text(text = item.bookName)
+                Text(text = item.bookWriter)
+                Text(text = item.PersonId)
+            }
+        }
 
-    // val bookList by view.list.collectAsState()
-         
-    val book = view.listBook.collectAsState(initial = emptyList())
-
-         Column (modifier = Modifier
-             .fillMaxSize()
-             .verticalScroll(rememberScrollState()))
-         {
+        // Other content below LazyColumn
+    }
 
 
-             Text(text = view.state.personCount.toString())
-                     AddPerson(view = view)
-                     DropMenu(view = view)
-                     AddBook(view = view)
-              
-             Text(text = book.value.toString())
-   
-         }
 
-          }
+}
        
     //   DropMenu(view = view)
 
@@ -78,6 +90,7 @@ Card(modifier = modifier) {
 
     )
     {
+        //val listBook = view.listBook.collectAsState(initial = emptyList()).value
         Text(text = "Add Book Which is read by Person")
         OutlinedTextField(
             value = view.state.bookName,
@@ -98,12 +111,39 @@ Card(modifier = modifier) {
     Button(onClick = { view.onEvent(mainScreenEvent.addBook) }) {
         Text(text = "Add Book")
     }
+
     }
 
 
 }
     
 }    
+
+@Composable
+fun test(view:mainScreenView){
+    val list=view.listBook.collectAsState(initial = emptyList())
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        // Other content above LazyColumn
+          AddPerson(view = view)
+        DropMenu(view = view)
+        AddBook(view = view)
+        LazyColumn(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+        ) {
+            items(list.value) { item ->
+                // LazyColumn items
+                Text(text = "Item ${item.bookName}")
+            }
+        }
+
+        // Other content below LazyColumn
+    }
+
+}
 
 @Composable
 fun AddPerson(
